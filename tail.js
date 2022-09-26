329681fe9a5bc5c064530d7d7d905d944bc20a72
minha0817 <minhakk24@gmail.com>
2022-09-26 08:45:18 -0700

    refactored tail.js - module & unit testing

--- tail.js
+++ tail.js
@@ -1,22 +1,5 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🥰 Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`😱 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const tail = function (words) {
-  console.log(words.slice(1));
+  return words.slice(1);
 };
 
-const words = ["Yo Yo"];
-tail(words);
-assertEqual(words.length, 3);
-
-x = 12;
-if (true) {
-  x + 2;
-  x = 4;
-}
-console.log(x);
+module.exports = tail;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,12 +1,19 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
+const tail = require("../tail");
 
+console.log("***** assertEqual test code");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(0, "0");
 assertEqual("minha", "minha");
 assertEqual(100, 1000);
 
+console.log("***** assertEqual & head test code");
 assertEqual(head([]), 0);
 assertEqual(head([0]), 0);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+console.log("***** assertEqual & tail test code");
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(tail(words).length, 3);
